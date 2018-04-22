329ab7e3d346c0aac95f099281f57bb454f1a2d3
Gaurav Tyagi <gauravtyagi888@gmail.com>
2018-04-22 11:48:18 -0700

    Added MySql COnnector file
    Signed-off-by: Gaurav Tyagi <gauravtyagi888@gmail.com>

--- MysqlConnector.py
+++ MysqlConnector.py
@@ -1,16 +1,19 @@
 import mysql.connector
 
-
 def get_connection():
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': '127.0.0.1',
-        'database': 'TEST_DB',
-        'raise_on_warnings': True,
-        'use_pure': False,
-    }
-    return mysql.connector.connect(**config)
+    try:
+        config = {
+            'user': 'root',
+            'password': 'root',
+            'host': '127.0.0.1',
+            'database': 'TEST_DB',
+            'raise_on_warnings': True,
+            'use_pure': False,
+        }
+        return mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+
 
 
 cnx = get_connection()
